cca74bfe5155cfbf4648f4e7fc35e9288b0b62f3
KIBA <kjh030529@gmail.com>
2022-06-08 19:24:29 +0900

    Add HeadReader, CanvasReader (not verified)

--- Runtime/Scripts/DataStructs/DataStructs.cs
+++ Runtime/Scripts/DataStructs/DataStructs.cs
@@ -1,44 +1,53 @@
+using System;
 using System.Collections.Generic;
 
 namespace KLOUD.GIFU
 {
+    public enum GIFVersion
+    {
+        a39 = 0x39,
+        a37 = 0x37
+    }
+
     public class DataStructs
     {
         public byte[] rawData;
         
-        public class Header
+        public class GIF_Header
         {
-            public string Sighnature = "GIF"; // Always (47="G", 49="I", 46="F")
-            public string Version = ""; // Always (38="8", 39="9",61="a") or (38="8", 37="7",61="a")
-        }
+            
+            public static string Sighnature = "GIF"; // Always (47="G", 49="I", 46="F")
+            public GIFVersion Version; // Always (38="8", 39="9",61="a") or (38="8", 37="7",61="a")
+        } public GIF_Header Header;
 
-        public class GlobalScreenDestriptor
+        public class GIF_GlobalScreenDestriptor
         {
-            public byte[] LogicalScreenWidth; // 2Byte
-            public byte[] LogicalScreenHeight; // 2Byte
+            public ushort CanvasWidth; // 2Byte
+            public ushort CanvasHeight; // 2Byte
 
             public class BitField
             {
-                public bool[] GlobalColorTableSize; // 3Byte
-                public bool ColorTableSortFlag;
-                public bool[] BitsPerPixel; // 3Byte
                 public bool GlobalColorTableFlag;
-                public byte BackgroundColor;
-                public byte PixelAspectRatio; // PixelAspectRatio == 0 ? 1:1 : 1:(PixelAspectRatio + 15)/64
+                public byte ColorResolution; // 3Bits
+                public bool SortFlag;
+                public byte Size; // 3Bits
             } public BitField BitFields = new  BitField();
-        }
+                
+            public byte BackgroundColor;
+            public byte PixelAspectRatio; // PixelAspectRatio == 0 ? 1:1 : 1:(PixelAspectRatio + 15)/64
+        } public GIF_GlobalScreenDestriptor GlobalScreenDestriptor = new GIF_GlobalScreenDestriptor();
 
-        public class GlobalColorTable
+        public class GIF_GlobalColorTable
         {
             public byte Red;
             public byte Green;
             public byte Blue;
-        }
+        } public GIF_GlobalColorTable GlobalColorTable = new GIF_GlobalColorTable();
 
-        public class ImageBlock
+        public class GIF_ImageBlock
         {
-            public byte ExtentionIntroducer; // Always (21)
-            public byte GraphicControlLabel; // Always (F9)
+            public static byte ExtentionIntroducer = 0x21; // Always (21)
+            public static byte GraphicControlLabel = 0xF9; // Always (F9)
             public byte ByteSize;
 
             public class BitField
@@ -49,37 +58,37 @@ namespace KLOUD.GIFU
                 public bool TransparentColorFlag;
             } public BitField BitFields = new  BitField();
 
-            public byte[] DelayTime; // 2Byte
+            public byte[] DelayTime = new byte[2]; // 2Byte
             public byte TransparentColorIndex;
-            public byte BlockTerminator; // Always (00)
-        }
+            public static byte BlockTerminator = 0x00; // Always (00)
+        } public GIF_ImageBlock ImageBlock = new GIF_ImageBlock();
 
-        public class ImageDescriptor
+        public class GIF_ImageDescriptor
         {
-            public byte ImageSeperator; // Always (2C)
-            public byte[] ImageLeft; // 2Byte
-            public byte[] ImageTop; // 2Byte
-            public byte[] ImageWidth; // 2Byte
-            public byte[] ImageHeight; // 2Byte
+            public static byte ImageSeperator = 0x2C; // Always (2C)
+            public byte[] ImageLeft = new byte[2]; // 2Byte
+            public byte[] ImageTop = new byte[2]; // 2Byte
+            public byte[] ImageWidth = new byte[2]; // 2Byte
+            public byte[] ImageHeight = new byte[2]; // 2Byte
 
             public class BitField
             {
                 public bool LocalColorTableFlag;
                 public bool InterlaceFlag;
                 public bool SortFlag;
-                public bool[] ReservedForFutureUse; // 2Byte
-                public bool[] SizeOfLocalColorTable; // 3Byte
+                public bool[] ReservedForFutureUse = new bool[2]; // 2Bits
+                public bool[] SizeOfLocalColorTable = new bool[3]; // 3Bits
             } public BitField BitFields = new  BitField();
-        }
+        } public GIF_ImageDescriptor ImageDescriptor = new GIF_ImageDescriptor();
 
-        public class LocalColorTable // Always Same GlobalColorTable
+        public class GIF_LocalColorTable // Always Same GlobalColorTable
         {
             public byte Red;
             public byte Green;
             public byte Blue;
-        }
+        } public GIF_LocalColorTable GifLocalColorTable = new GIF_LocalColorTable();
 
-        public class ImageData
+        public class GIF_ImageData
         {
             public byte LZWMinimumCodeSize;
 
@@ -89,13 +98,13 @@ namespace KLOUD.GIFU
                 public byte[] data;
             } public List<Data> Datas = new  List<Data>();
 
-            public byte BlockTerminator; // Always (00)
-        }
+            public static byte BlockTerminator = 0x00; // Always (00)
+        } public GIF_ImageData GifImageData = new GIF_ImageData();
 
         public byte[] PlainTextExtension;
         public byte[] ApplicationExtension;
         public string CommentExtension;
 
-        public byte Trailer; // Always (3B)
+        public static byte Trailer = 0x3B; // Always (3B)
     }
 }
---
+++ Runtime/Scripts/RawGifData.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using Unity.Plastic.Newtonsoft.Json.Converters;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+namespace KLOUD.GIFU
+{
+
+    public class RawGifData
+    {
+        private DataStructs Data = new DataStructs();
+
+        public byte[] RawData
+        {
+            set
+            {
+                Load(value);
+            }
+
+            get => Data.rawData;
+        }
+
+        public RawGifData(byte[] data)
+        {
+            RawData = data;
+            Load(data);
+        }
+
+        public void Load(byte[] data)
+        {
+            Data.rawData = data;
+            
+            ReadHeader();
+            ReadCanvasDescriptor();
+            
+        }
+
+        public void ReadHeader() // 0~5 bytes
+        {
+            var Sighnature = true;
+            for(int i = 0; i< 3; i++) Sighnature &= Data.rawData[i] == DataStructs.GIF_Header.Sighnature[i];
+
+            if (!Sighnature) Debug.LogError("[GIFU] Is not GIF format");
+
+            Data.Header.Version = (GIFVersion) Data.rawData[4];
+        }
+
+        public void ReadCanvasDescriptor() // 6~12 bytes
+        {
+           Data.GlobalScreenDestriptor.CanvasWidth = BitConverter.ToUInt16(new byte[2] {DataStructs.rawData[6], DataStructs.rawData[7]});
+           Data.GlobalScreenDestriptor.CanvasHeight = BitConverter.ToUInt16(new byte[2] {DataStructs.rawData[8], DataStructs.rawData[9]});
+           var bits = new BitArray(DataStructs.rawData[10]);
+           Data.GlobalScreenDestriptor.BitFields.GlobalColorTableFlag = bits[0];
+           Data.GlobalScreenDestriptor.BitFields.ColorResolution =
+               ReadUtil.ConvertBitsToByte(ReadUtil.ConvertBoolsToBits(new bool[] {bits[0], bits[0], bits[0]}));
+           Data.GlobalScreenDestriptor.BitFields.SortFlag = bits[4];
+           Data.GlobalScreenDestriptor.BitFields.Size = ReadUtil.ConvertBitsToByte(ReadUtil.ConvertBoolsToBits(new bool[]{ bits[5], bits[6], bits[7] }));
+           Data.GlobalScreenDestriptor.BackgroundColor = DataStructs.rawData[11];
+           Data.GlobalScreenDestriptor.PixelAspectRatio = DataStructs.rawData[12];
+        }
+    }
+}
